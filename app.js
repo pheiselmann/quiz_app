ba93ef88af7d7a00e4d8af2871b666e9a858a197
pheiselmann <prhmann@gmail.com>
2017-05-01 14:27:19 -0700

    checked radio value

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 $(function() {
 $("#quiz-form").submit(function(event) {
-  event.preventDefault();\
+  event.preventDefault();
   var answer = $("form input[type='radio']:checked").val();
   //$(this).children("span .message").text("woohoo!");
   alert("Value of Submitted Radio is: " + answer);
